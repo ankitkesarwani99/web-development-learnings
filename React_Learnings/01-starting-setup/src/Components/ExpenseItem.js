1d36b41a8749d6579f6bc3e5aed4caec22bd556e
ankitkesarwani99 <ankit.kesarwani@meesho.com>
2022-02-17 23:40:21 +0530

    pacticing and learning props,events and state in react

---
+++ React_Learnings/01-starting-setup/src/Components/ExpenseItem.js
@@ -0,0 +1,31 @@
+import './ExpenseItem.css';
+
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import {useState} from 'react'
+
+function ExpenseItem(props) {
+
+    const [newTitle,setNewTitle]=useState("");
+
+    const [title,setTitle]=useState(props.title);
+    const clickHandler=()=>{
+        setTitle(newTitle);
+    }
+
+    const changeHandler=(event)=>{
+        setNewTitle(event.target.value);
+    }
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+            <input type="text" value={newTitle} onChange={ changeHandler }></input>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
+    )
+}
+export default ExpenseItem;
